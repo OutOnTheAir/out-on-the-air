51c45e6ddc6f6f4a655b0149e2f54d877da1b638
OutOnTheAir <outontheair@outlook.com>
2026-03-18 13:55:58 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import StatsBar from '@/components/StatsBar'
 import SpotsBoard from '@/components/SpotsBoard'
-import WhyOOTA from '@/components/WhyOOTA'
-import TaglineStrip from '@/components/TaglineStrip'
 import Footer from '@/components/Footer'
 
 export default function Home() {
@@ -13,8 +11,6 @@ export default function Home() {
       <Hero />
       <StatsBar />
       <SpotsBoard />
-      <WhyOOTA />
-      <TaglineStrip />
       <Footer />
     </main>
   )
