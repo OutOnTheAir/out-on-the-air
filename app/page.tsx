dd51639f36f33ce371d772880fcc8eaadd3e437c
OutOnTheAir <outontheair@outlook.com>
2026-03-21 12:56:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,3 @@ export default function Home() {
     </main>
   )
 }
-import Nav from '@/components/Nav'
-import Hero from '@/components/Hero'
-import StatsBar from '@/components/StatsBar'
-import SpotsBoard from '@/components/SpotsBoard'
-import Footer from '@/components/Footer'
-
-export default function Home() {
-  return (
-    <main>
-      <Nav />
-      <Hero />
-      <StatsBar />
-      <SpotsBoard />
-      <Footer />
-    </main>
-  )
-}
