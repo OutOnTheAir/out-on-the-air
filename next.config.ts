511c1be5bc4f6d30497fb7c457305357a4545192
OutOnTheAir <outontheair@outlook.com>
2026-05-14 10:34:35 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/spot/new',
+        destination: '/log/new',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
